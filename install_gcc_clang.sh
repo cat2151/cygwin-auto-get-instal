fd1d4d85f7ccbe7b863312ba1aa29901ec59dca6
cat2151 <fct_2004@mail.goo.ne.jp>
2022-11-26 16:13:01 +0900

    Cygwin公式のdepends定義仕様変更に apt-get が対応したので、不要となった installGccDepends を削除

--- install_gcc_clang.sh
+++ install_gcc_clang.sh
@@ -20,6 +20,7 @@ installAptCyg() {
 }
 
 installAptCygSub() {
+  # [前提] Cygwin_get_and_install.bat の installCygwin にて必須packageがinstall済みであること。なおこの関数にその処理の集約を試みたが中断した（$packageSite を持ってくる等かえって複雑になりそうなため）
   wget https://raw.githubusercontent.com/kou1okada/apt-cyg/master/apt-cyg
   chmod 755 apt-cyg
   mv apt-cyg /usr/local/bin/
@@ -58,86 +59,6 @@ installCpanmSub() {
   curl -LOk http://xrl.us/cpanm
   chmod.exe +x cpanm
   cpanm --version
-}
-
-installGccDepends() {
-  # 各toolを動かすため、toolが依存しているパッケージを明示的にinstallする用。以前はこのshを使う方式であっても自動的にinstallされていた。今はこのように明示的なinstallをしないとinstallされない。経緯は不明。
-
-  # 必須ヘッダを入手する用。gccやclangを使ってのコンパイルに必須。これに気づくには、ぐぐって基礎知識を得て、公式 setup-x86_64.exe のinstall listをみて automatically added されているものがあることを知る必要があった。
-  apt-cyg install cygwin-devel            # stdio.h  など
-  apt-cyg install mingw64-x86_64-headers  # stddef.h など
-
-  # mingw64-x86_64-gcc-core が依存しているもの : https://www.cygwin.com/packages/summary/mingw64-x86_64-gcc-core.html より
-  apt-cyg install bash
-  apt-cyg install cygwin
-  apt-cyg install libgmp10
-  apt-cyg install libiconv2
-  apt-cyg install libintl8
-  apt-cyg install libisl23
-  apt-cyg install libmpc3
-  apt-cyg install libmpfr6
-  apt-cyg install libzstd1
-  apt-cyg install mingw64-x86_64-binutils
-  apt-cyg install mingw64-x86_64-runtime
-  apt-cyg install mingw64-x86_64-windows-default-manifest
-  apt-cyg install mingw64-x86_64-winpthreads
-  apt-cyg install zlib0
-
-  # gdbが依存しているもの : https://cygwin.com/packages/summary/gdb.html より
-  apt-cyg install bash
-  apt-cyg install cygwin
-  apt-cyg install libexpat1
-  apt-cyg install libgcc1
-  apt-cyg install libgmp10
-  apt-cyg install libiconv
-  apt-cyg install libiconv2
-  apt-cyg install libintl8
-  apt-cyg install liblzma5
-  apt-cyg install libmpfr6
-  apt-cyg install libncursesw10
-  apt-cyg install libreadline7
-  apt-cyg install libsource-highlight4
-  apt-cyg install libstdc++6
-  apt-cyg install python39
-  apt-cyg install zlib0
-  # 公式pageには不足があり、cygcheck gdb で確認したところ、以下にも依存していた
-  apt-cyg install libboost_regex1.66
-  apt-cyg install libicu61
-
-  # makeが依存しているもの : https://cygwin.com/packages/summary/make.html より
-  apt-cyg install cygwin
-  apt-cyg install libguile2.2_1
-  apt-cyg install libintl8
-  # 公式pageには不足があり、cygcheck make で確認したところ、以下にも依存していた
-  apt-cyg install libgc1
-  apt-cyg install libltdl7
-
-  # cmakeが依存しているもの : https://cygwin.com/packages/summary/cmake.html より
-  apt-cyg install bash
-  apt-cyg install cygwin
-  apt-cyg install libarchive13
-  apt-cyg install libcurl4
-  apt-cyg install libexpat1
-  apt-cyg install libgcc1
-  apt-cyg install libjsoncpp25
-  apt-cyg install libncursesw10
-  apt-cyg install librhash0
-  apt-cyg install libstdc++6
-  apt-cyg install libuuid1
-  apt-cyg install libuv1
-  apt-cyg install zlib0
-  # 公式pageには不足があり、cygcheck cmake で確認したところ、以下にも依存していた
-  apt-cyg install liblzo2_2
-
-  # clangが依存しているもの : https://www.cygwin.com/packages/summary/mingw64-x86_64-clang.html より
-  apt-cyg install libclang8
-  apt-cyg install mingw64-x86_64-binutils
-  apt-cyg install mingw64-x86_64-gcc-core
-  apt-cyg install mingw64-x86_64-gcc-g++
-  apt-cyg install mingw64-x86_64-runtime
-  # 公式pageには不足があり、x86_64-w64-mingw32-clang の起動エラーメッセージで確認したところ、以下にも依存していた（cygcheck x86_64-w64-mingw32-clangはエラーとなった）
-  apt-cyg install libllvm8
-  apt-cyg install libpolly8
 }
 
 installGccMingw() {
@@ -230,7 +151,7 @@ addAliasToProfile() {
   # bashログインしgccやclangをタイプし、(mingwの)gccやclangがインストールされているか確認できる用
   echo '#'>> ~/.bash_profile
   echo 'alias gcc="echo （CygwinのgccはDLL依存のため、かわりにx86_64-w64-mingw32-gccを使います）;x86_64-w64-mingw32-gcc"'>> ~/.bash_profile
-  echo 'alias g++="echo （CygwinのgccはDLL依存のため、かわりにx86_64-w64-mingw32-g++を使います）;x86_64-w64-mingw32-g++"'>> ~/.bash_profile
+  echo 'alias g++="echo （Cygwinのg++はDLL依存のため、かわりにx86_64-w64-mingw32-g++を使います）;x86_64-w64-mingw32-g++"'>> ~/.bash_profile
   echo 'alias clang="echo （CygwinのclangはDLL依存のため、かわりにx86_64-w64-mingw32-clangを使います）;x86_64-w64-mingw32-clang"'>> ~/.bash_profile
   echo 'alias clang++="echo （Cygwinのclang++はDLL依存のため、かわりにx86_64-w64-mingw32-clang++を使います）;x86_64-w64-mingw32-clang++"'>> ~/.bash_profile
 }
@@ -240,7 +161,6 @@ main() {
   installAptCyg
   installPerl
   installCpanm
-  installGccDepends
   installGccMingw
   installClangMingw
   rebaseall # exeを実行するとエラーになる問題の対策用（CygwinとWindowsの環境依存不具合）
